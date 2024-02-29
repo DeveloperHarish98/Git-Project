bd04ee095d06a525142a32c53e0816504ec23fe2
DeveloperHarish98 <harishdubey98r@gmail.com>
2024-02-29 12:23:44 +0530

    I have added code for add 2 numbers

---
+++ AddTwoNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class AddTwoNumbers {
+  public static void main(String[] args) {
+    int num1;
+    int num2;
+    int sum;
+
+   Scanner sc = new Scanner
+    System.out.println("Enter a number:");
+    x = sc.nextInt(); 
+
+    System.out.println("Enter another number:");
+    y = sc.nextInt();
+
+    sum = num1 + num2; 
+    System.out.println("Sum of two numbers is: " + sum);
+  }
+} 
